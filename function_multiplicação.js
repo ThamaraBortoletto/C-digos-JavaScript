a1c8384e46ae6d182d9730dc30dadea42c31613b
ThamaraBortoletto <thamarabortoletto@hotmail.com>
2022-06-21 14:21:47 -0300

    function_multiplicação

---
+++ function_multiplicação.js
@@ -0,0 +1,18 @@
+function multiplicação(num01, num02) {
+    let resultado = num01 * num02;
+    console.log(resultado);
+
+    if(resultado > 100) {
+        console.log("numero maior que 100 :)");
+    }
+
+    else {
+        console.log("numero menor que 100 :)");
+    }
+
+}
+
+let conta = multiplicação(10, 40)
+let conta02 = multiplicação(2, 7)
+let conta03 = multiplicação(40, 1)
+console.log(`${conta} e ${conta02} e ${conta03}`);
